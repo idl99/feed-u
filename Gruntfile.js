949c2eb36c24f8be72a750fa3ba991d4a58c6c11
Ihan Dilnath <ihandilnath2012@gmail.com>
2020-12-28 00:06:22 +0530

    add livereload and migrate from grunt-copy to grunt-sync

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,21 +2,6 @@ module.exports = function (grunt) {
 
     grunt.initConfig({
         clean: ['dist/'], // Deletes the dist folder to start from a clean state
-        copy: { // Copies pages to the dist folder
-            main: {
-                files: [{
-                    expand: true,
-                    cwd: 'src/',
-                    src: ['pages/*'],
-                    dest: 'dist/'
-                }, {
-                    expand: true,
-                    cwd: 'src/',
-                    src: ['assets/*/*'],
-                    dest: 'dist/'
-                }]
-            },
-        },
         sass: { // Compiles SASS into CSS and copies them to the dist folder
             dist: {
                 files: [{
@@ -34,16 +19,62 @@ module.exports = function (grunt) {
                 src: './src/scripts/index.js',
                 dest: 'dist/bundle.js'
             }
-        }
+        },
+        connect: { // Starts a static file server
+            server: {
+                options: {
+                    hostname: 'localhost',
+                    port: 3000,
+                    base: 'dist',
+                    livereload: true
+                }
+            }
+        },
+        sync: {
+            // Keeps directories in sync and
+            // compared to grunt-contrib-copy, sync only copies newly changed files
+            main: {
+                files: [
+                    { expand: true, cwd: 'src/', src: ['pages/*'], dest: 'dist' },
+                    { expand: true, cwd: 'src/', src: ['assets/**'], dest: 'dist' }
+                ],
+                verbose: true // Display log messages when copying files
+                // pretend: true, // This option prevents any IO. dry run.
+            }
+        },
+        watch: {
+            options: {
+                livereload: true
+            },
+            js: {
+                files: ['src/scripts/*.js'],
+                tasks: ['browserify']
+            },
+            css: {
+                files: ['src/styles/*.sass'],
+                tasks: ['sass']
+            },
+            html: {
+                files: ['src/pages/*.html'],
+                tasks: ['sync']
+            },
+            assets: {
+                files: ['src/assets/**'],
+                tasks: ['sync']
+            }
+        },
     });
 
     // https://gruntjs.com/plugins
     grunt.loadNpmTasks('grunt-contrib-clean')
-    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-browserify')
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-sync');
 
     // Default task
-    grunt.registerTask('default', ['clean', 'copy', 'sass', 'browserify']);
+    grunt.registerTask('build', ['clean', 'sync', 'sass', 'browserify']);
+    grunt.registerTask('dev', ['build', 'connect', 'watch']);
 
 };
