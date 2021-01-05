8c96f3794f732094bf0f29b7745403374c4238dd
Deshan <dehami.deshan@gmail.com>
2021-01-05 09:45:48 +0530

    Add logic to save the vendor and item ratings

--- src/scripts/datastore.js
+++ src/scripts/datastore.js
@@ -1,4 +1,4 @@
-const data = Object.freeze({
+const data = {
     vendors: [
         {
             "vendorId": "v1",
@@ -25,41 +25,28 @@ const data = Object.freeze({
                         "itemNoOfRatings": 47
                     },
                     "itemPrice": 800
-                },
-                // TODO remove duplicate item here
-                {
-                    "itemImageLocation": "../assets/vendor_page_images/tom_yum_soup.jpg",
-                    "itemId": "v1i2",
-                    "itemTitle": "Tom Yum Soup",
-                    "itemDescription": "Picked from the fresh vegetables and fish from the with special Chicken Broth.",
-                    "itemRatings": {
-                        "itemRatingValue": 4.6,
-                        "itemNoOfRatings": 47
-                    },
-                    "itemPrice": 800
                 }
             ],
         },
         {
-            "vendorId": "v2",
-            "vendorImageLocation": "../assets/vendor_list_view_images/pizza_palpatha.jpg",
-            "vendorName": "Pizza Palpatha",
+            "vendorId": "v1",
+            "vendorImageLocation": "../assets/vendor_list_view_images/burgers_ahouy.jpg",
+            "vendorName": "Burgers Ahouy!",
             "vendorCatergories": [
-                "Italian",
+                "Burgers",
                 "American",
-                "Pizza"
+                "Mexican"
             ],
             "vendorRatings": {
-                "vendorRatingValue": 4.8,
-                "vendorNoOfRatings": 99
+                "vendorRatingValue": 4.6,
+                "vendorNoOfRatings": 100
             },
-            "location": "Colombo 4",
-            "deliveryTime": "33-40 mins",
+            "location": "Colombo 3",
+            "deliveryTime": "25-30 mins",
             "items": [
-                // TODO remove duplicate items here
                 {
                     "itemImageLocation": "../assets/vendor_page_images/tom_yum_soup.jpg",
-                    "itemId": "v2i1",
+                    "itemId": "v1i1",
                     "itemTitle": "Tom Yum Soup",
                     "itemDescription": "Picked from the fresh vegetables and fish from the with special Chicken Broth.",
                     "itemRatings": {
@@ -71,60 +58,7 @@ const data = Object.freeze({
             ],
         },
     ],
-
-    pastOrders: [
-        {
-            "pastOrderId": "ord1",
-            "vendorCoverImageLocation": "",
-            "vendorLogoImageLocation": "../assets/stories_images/story-big.png",
-            "vendorName": "Burger Ahoy!",
-            "noOfOrderedItems": 3,
-            "price": 1000,
-            "date": "Oct 10 at 12:12 PM",
-            "vendorRating": 0,
-            "vendorComment": "",
-            "orderedItems": [
-                {
-                    "itemName": "Hawaiian Burger",
-                    "quantity": 2,
-                    "rating": 0,
-                    "comment": ""
-                },
-                {
-                    "itemName": "Pudding",
-                    "quantity": 2,
-                    "rating": 0,
-                    "comment": ""
-                },
-                {
-                    "itemName": "Special Chilli Sauce",
-                    "quantity": 1,
-                    "rating": 0,
-                    "comment": ""
-                }
-            ]
-        },
-        {
-            "pastOrderId": "ord2",
-            "vendorCoverImageLocation": "",
-            "vendorLogoImageLocation": "../assets/stories_images/story-big.png",
-            "vendorName": "Burger Ahoy!",
-            "noOfOrderedItems": 1,
-            "price": 500,
-            "date": "Oct 01 at 08:30 PM",
-            "vendorRating": 4,
-            "vendorComment": "The food is great but the quantity is not enough.",
-            "orderedItems": [
-                {
-                    "itemName": "Fish and Chips Burger",
-                    "quantity": 2,
-                    "rating": 4,
-                    "comment": "Great food. Please increase on the quantity a bit."
-                },
-            ]
-        },
-    ]
-});
+}
 
 module.exports = function () {
     return {
@@ -141,13 +75,6 @@ module.exports = function () {
             const allVendorsItems = data.vendors.flatMap(vendor => vendor.items)
             const item = allVendorsItems.find(item => item.itemId === itemId);
             return item;
-        },
-        getAllOrders: function () {
-            return data.pastOrders;
-        },
-        getSpecificOrder: function (pastOrderId) {
-            const order = data.pastOrders.find(order => order.pastOrderId == pastOrderId);
-            return order;
         }
     }
 }()
--- src/scripts/user.js
+++ src/scripts/user.js
@@ -2,6 +2,61 @@ var cookie = require('js-cookie');
 
 const USER = "user";
 
+const orders = {
+    pastOrders: [
+        {
+            "pastOrderId": "ord1",
+            "vendorCoverImageLocation": "",
+            "vendorLogoImageLocation": "../assets/stories_images/story-big.png",
+            "vendorName": "Burger Ahoy!",
+            "noOfOrderedItems": 3,
+            "price": 1000,
+            "date": "Oct 10 at 12:12 PM",
+            "vendorRating": 0,
+            "vendorComment": "",
+            "orderedItems": [
+                {
+                    "itemName": "Hawaiian Burger",
+                    "quantity": 2,
+                    "rating": 0,
+                    "comment": ""
+                },
+                {
+                    "itemName": "Pudding",
+                    "quantity": 2,
+                    "rating": 0,
+                    "comment": ""
+                },
+                {
+                    "itemName": "Special Chilli Sauce",
+                    "quantity": 1,
+                    "rating": 0,
+                    "comment": ""
+                }
+            ]
+        },
+        {
+            "pastOrderId": "ord2",
+            "vendorCoverImageLocation": "",
+            "vendorLogoImageLocation": "../assets/stories_images/story-big.png",
+            "vendorName": "Burger Ahoy!",
+            "noOfOrderedItems": 1,
+            "price": 500,
+            "date": "Oct 01 at 08:30 PM",
+            "vendorRating": 4,
+            "vendorComment": "The food is great but the quantity is not enough.",
+            "orderedItems": [
+                {
+                    "itemName": "Fish and Chips Burger",
+                    "quantity": 2,
+                    "rating": 4,
+                    "comment": "Great food. Please increase on the quantity a bit."
+                },
+            ]
+        },
+    ]
+}
+
 module.exports = function () {
     return {
         init: function () {
@@ -14,6 +69,41 @@ module.exports = function () {
         },
         getDetails: function () {
             return cookie.getJSON(USER)
+        },
+        getAllOrders: function () {
+            return orders.pastOrders;
+        },
+        getSpecificOrder: function (pastOrderId) {
+            const order = orders.pastOrders.find(order => order.pastOrderId == pastOrderId);
+            return order;
+        },
+        updateSpecificOrder: function (pastOrderId, updatedVendorRating, updatedVendorComment,
+            updatedItemNames, updateItemRatings, updatedItemComments) {
+
+            const order = orders.pastOrders.find(order => order.pastOrderId == pastOrderId);
+            // Update the vendor rating and comment.
+            order.vendorRating = updatedVendorRating
+            order.vendorComment = updatedVendorComment
+
+            // Update the item raings and comments.
+            var updatedOrderItems = [];
+            for (var i = 0; i < updatedItemNames.length; i++) {
+                order.orderedItems.map(item => {
+                    if (item.itemName.replace(/\s/g, "") === updatedItemNames[i]) {
+                        updatedOrderItems.push({
+                            "itemName": item.itemName,
+                            "quantity": item.quantity,
+                            "rating": updateItemRatings[i],
+                            "comment": "",
+                            // "comment": updatedItemComments[i] // TODO - Comments part Thenuka
+                        });
+                    }
+                });
+            }
+            order.orderedItems = updatedOrderItems;
+
+            var test = orders.pastOrders.find(order => order.pastOrderId == pastOrderId);
+            console.log("Updated Order:", test);
         }
     }
 }()
