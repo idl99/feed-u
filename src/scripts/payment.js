1642c63eb71332ab186ac4ce305a95f37f04c594
Ihan Dilnath <ihandilnath2012@gmail.com>
2021-01-05 23:16:48 +0530

    behaviour to remove payment method and refactored payment.js code that makes API calls

--- src/scripts/payment.js
+++ src/scripts/payment.js
@@ -1,12 +1,9 @@
-// // Stripe API (Node.js)
-// const Stripe = require('stripe');
-// const stripeAPI = Stripe('sk_test_51I3c8WBYsXgRXg4suIS6WCRMMcfMsNgopWokzuqiKXSo6pmVX00zIk7Up6ukdjZLEY6YfpclJ1lAUCBKpl6Y4fe600CfNE584P');
-
 const $ = require('jquery');
+const loggedInUser = require('./user').getPaymentDetails();
 
 const SECRET_KEY = 'sk_test_51I3c8WBYsXgRXg4suIS6WCRMMcfMsNgopWokzuqiKXSo6pmVX00zIk7Up6ukdjZLEY6YfpclJ1lAUCBKpl6Y4fe600CfNE584P';
 
-const postToStripeAPI = function (url, data, success, error) {
+const postToStripeAPI = function ({ url, data, success, error }) {
     return $.ajax({
         beforeSend: function (xhr) {
             xhr.setRequestHeader('Authorization', `Bearer ${SECRET_KEY}`);
@@ -20,7 +17,7 @@ const postToStripeAPI = function (url, data, success, error) {
     })
 }
 
-const getFromStripeAPI = function (url, queryParams, success = () => { }, error = () => { }) {
+const getFromStripeAPI = function ({ url, queryParams, success, error }) {
     return $.ajax({
         beforeSend: function (xhr) {
             xhr.setRequestHeader('Authorization', `Bearer ${SECRET_KEY}`);
@@ -37,15 +34,35 @@ const getFromStripeAPI = function (url, queryParams, success = () => { }, error 
 module.exports = function () {
     return {
         createSessionToSaveCardDetails: function (options, success, error) {
-            return postToStripeAPI('checkout/sessions', {
-                payment_method_types: ['card'],
-                mode: 'setup', // only setup the payment method, without charging any initial payment
-                ...options
-            }, success, error)
+            return postToStripeAPI({
+                url: 'checkout/sessions',
+                data: {
+                    payment_method_types: ['card'],
+                    mode: 'setup', // only setup the payment method, without charging any initial payment
+                    ...options
+                },
+                success,
+                error
+            })
         },
-        getAllCardsForCustomer: function (customer, success, error) {
+        getAllCardsForCustomer: function (success, error) {
             // https://stripe.com/docs/api/payment_methods/list?lang=curl
-            return getFromStripeAPI('payment_methods', { customer, type: 'card' }, success, error);
+            return getFromStripeAPI({
+                url: 'payment_methods',
+                queryParams: {
+                    customer: loggedInUser.stripe_customer_id,
+                    type: 'card'
+                },
+                success,
+                error
+            });
+        },
+        removeCard: function (cardId, success, error) {
+            return postToStripeAPI({
+                url: `payment_methods/${cardId}/detach`,
+                success,
+                error
+            });
         }
     }
 }()
