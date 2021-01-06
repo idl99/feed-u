7e27c0178409526b0545b60531b725bb4b5805d2
Ihan Dilnath <ihandilnath2012@gmail.com>
2021-01-07 00:18:06 +0530

    Fix Google Maps API issue ... "trying to load twice"

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -20,3 +20,15 @@ window.app = {
     utils,
     payment,
 }
+
+/**
+ * Remove Google Maps from the document before hiding the current page to load the next.
+ * Since we are using AJAX navigation between the pages, Google will show us an error 
+ * indicating that we are trying to load the Google Maps API script multiple times.
+ * The code given below prevents that issue by resetting the source code set on the window (window.google)
+ * each time the Google Maps API script is loaded.
+ */
+$(document).on("pagecontainerbeforehide", function (event, ui) {
+    console.log('Page going to hide ... removing Google Maps scripts')
+    window.google = undefined;
+})
