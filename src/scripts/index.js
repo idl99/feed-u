0d82a082ee1259458e5e45ce066059ef233c4b65
Ihan Dilnath <ihandilnath2012@gmail.com>
2021-01-09 01:33:41 +0530

    refactor to use external panel for navigation instead of internal panel

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -41,21 +41,11 @@ $(document).on("pagecontainerbeforehide", function (event, ui) {
  * https://github.com/jquery/demos.jquerymobile.com/blob/master/1.4.5/panel-external/index.html
  */
 $(document).one("ready", function () {
+    $.get("navigation_side_bar.html", function (markup) {
+        // Add the panel to the body
+        $('body').append(markup);
 
-    var panelMarkup = `
-        <div data-role="panel" id="leftpanel1" data-position="left" data-display="reveal" data-theme="a">
-
-        <h3>Left Panel: Reveal</h3>
-        <p>This panel is positioned on the left with the reveal display mode. The panel markup is <em>after</em> the header, content and footer in the source order.</p>
-        <p>To close, click off the panel, swipe left or right, hit the Esc key, or use the button below:</p>
-        <a href="#demo-links" data-rel="close" class="ui-btn ui-shadow ui-corner-all ui-btn-a ui-icon-delete ui-btn-icon-left ui-btn-inline">Close panel</a>
-
-        </div>
-    `
-
-    // Add the panel to the body
-    $('body').append(panelMarkup);
-
-    // Manually initialize the panel
-    $("body>[data-role='panel']").panel();
+        // Manually initialize the panel
+        $("body>[data-role='panel']").panel();
+    })
 })
