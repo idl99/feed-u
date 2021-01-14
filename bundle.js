d3fa1fb4b5f2d4ecd434068469d33810775a6002
deshankoswatte <dehami.deshan@gmail.com>
2021-01-14 09:52:36 +0000

    Deploying to gh-pages from  @ 1845a67c1517a9bbfd9a1341f8327f30712c1c1b 🚀

--- bundle.js
+++ bundle.js
@@ -419,6 +419,28 @@ const data = Object.freeze({
                         "itemNoOfRatings": 47
                     },
                     "itemPrice": 800
+                },
+                {
+                    "itemImageLocation": "../assets/vendor_page_images/crispy_chicken_burger.jpg",
+                    "itemId": "v1i2",
+                    "itemTitle": "Crispy Chicken Burger",
+                    "itemDescription": "Crispy, Chicky, Crispy Chicken Burger. It’s pretty much self-explanatory.",
+                    "itemRatings": {
+                        "itemRatingValue": 4.0,
+                        "itemNoOfRatings": 10
+                    },
+                    "itemPrice": 450
+                },
+                {
+                    "itemImageLocation": "../assets/vendor_page_images/hawaiian_burger.jpg",
+                    "itemId": "v1i3",
+                    "itemTitle": "Hawaiian Burger",
+                    "itemDescription": "Yes! Pineapples go on burgers too. Don’t let others tell you otherwise.",
+                    "itemRatings": {
+                        "itemRatingValue": 4.8,
+                        "itemNoOfRatings": 45
+                    },
+                    "itemPrice": 600
                 }
             ],
         },
@@ -435,19 +457,41 @@ const data = Object.freeze({
                 "vendorRatingValue": 4.8,
                 "vendorNoOfRatings": 99
             },
-            "location": "Colombo 3",
+            "location": "Colombo 5",
             "deliveryTime": "15-20 mins",
             "items": [
                 {
-                    "itemImageLocation": "../assets/vendor_page_images/tom_yum_soup.jpg",
-                    "itemId": "v1i1",
-                    "itemTitle": "Tom Yum Soup",
-                    "itemDescription": "Picked from the fresh vegetables and fish from the with special Chicken Broth.",
+                    "itemImageLocation": "../assets/vendor_page_images/cheese_lovers_pizza.jpg",
+                    "itemId": "v2i1",
+                    "itemTitle": "Cheese Lovers Pizza",
+                    "itemDescription": "Rich tomato sauce with a triple layer of mozzarella cheese.",
                     "itemRatings": {
-                        "itemRatingValue": 4.6,
-                        "itemNoOfRatings": 47
+                        "itemRatingValue": 4.5,
+                        "itemNoOfRatings": 55
                     },
-                    "itemPrice": 800
+                    "itemPrice": 500
+                },
+                {
+                    "itemImageLocation": "../assets/vendor_page_images/chicken_bacon_pizza.jpg",
+                    "itemId": "v2i2",
+                    "itemTitle": "Chicken Bacon Pizza",
+                    "itemDescription": "Chicken bacon, onions & green chillies with a double layer of mozzarella cheese.",
+                    "itemRatings": {
+                        "itemRatingValue": 4.9,
+                        "itemNoOfRatings": 90
+                    },
+                    "itemPrice": 300
+                },
+                {
+                    "itemImageLocation": "../assets/vendor_page_images/devilled_chicken_pizza.jpg",
+                    "itemId": "v2i3",
+                    "itemTitle": "Devilled Chicken Pizza",
+                    "itemDescription": "Devilled chicken in spicy sauce with a double layer of mozzarella cheese.",
+                    "itemRatings": {
+                        "itemRatingValue": 4.1,
+                        "itemNoOfRatings": 75
+                    },
+                    "itemPrice": 470
                 }
             ],
         },
@@ -463,20 +507,92 @@ const data = Object.freeze({
                 "vendorRatingValue": 4.5,
                 "vendorNoOfRatings": 150
             },
-            "location": "Colombo 3",
+            "location": "Colombo 7",
             "deliveryTime": "5-10 mins",
             "items": [
                 {
-                    "itemImageLocation": "../assets/vendor_page_images/tom_yum_soup.jpg",
-                    "itemId": "v1i1",
-                    "itemTitle": "Tom Yum Soup",
-                    "itemDescription": "Picked from the fresh vegetables and fish from the with special Chicken Broth.",
+                    "itemImageLocation": "../assets/vendor_page_images/prawn_curry_and_rice.jpg",
+                    "itemId": "v3i1",
+                    "itemTitle": "Prawn Curry and Rice",
+                    "itemDescription": "Prawn curry prepared to special Goa recipe with thick coconut gravy.",
                     "itemRatings": {
-                        "itemRatingValue": 4.6,
-                        "itemNoOfRatings": 47
+                        "itemRatingValue": 4.2,
+                        "itemNoOfRatings": 21
                     },
-                    "itemPrice": 800
-                }
+                    "itemPrice": 575
+                },
+                {
+                    "itemImageLocation": "../assets/vendor_page_images/fish_rice_and_curry.jpg",
+                    "itemId": "v3i2",
+                    "itemTitle": "Fish Rice and Curry",
+                    "itemDescription": "Thalapath Fish, 4 curries and papadam enhriched with the Sri Lankan taste.",
+                    "itemRatings": {
+                        "itemRatingValue": 4.2,
+                        "itemNoOfRatings": 21
+                    },
+                    "itemPrice": 575
+                },
+                {
+                    "itemImageLocation": "../assets/vendor_page_images/chicken_rice_and_curry.jpg",
+                    "itemId": "v3i3",
+                    "itemTitle": "Chicken Rice and Curry",
+                    "itemDescription": "With 4 curries and papadam mixed with Sri Lankan spices.",
+                    "itemRatings": {
+                        "itemRatingValue": 4.4,
+                        "itemNoOfRatings": 65
+                    },
+                    "itemPrice": 350
+                },
+            ],
+        },
+        {
+            "vendorId": "v4",
+            "vendorImageLocation": "../assets/vendor_list_view_images/the_impasta.jpg",
+            "vendorName": "The Impasta",
+            "vendorCatergories": [
+                "Pasta",
+                "Italian"
+            ],
+            "vendorRatings": {
+                "vendorRatingValue": 4.1,
+                "vendorNoOfRatings": 82
+            },
+            "location": "Colombo 1",
+            "deliveryTime": "25-40 mins",
+            "items": [
+                {
+                    "itemImageLocation": "../assets/vendor_page_images/bruschetta_pasta.jpg",
+                    "itemId": "v4i1",
+                    "itemTitle": "Brushcetta Pasta",
+                    "itemDescription": "This Bruschetta Pasta with fresh tomatoes, basil, garlic, and olive oil.",
+                    "itemRatings": {
+                        "itemRatingValue": 4.0,
+                        "itemNoOfRatings": 10
+                    },
+                    "itemPrice": 375
+                },
+                {
+                    "itemImageLocation": "../assets/vendor_page_images/cheese_pasta.jpg",
+                    "itemId": "v4i2",
+                    "itemTitle": "Cheese Pasta",
+                    "itemDescription": "Just remember that a single is a double and a double is a triple at Chicago’s Au Cheval.",
+                    "itemRatings": {
+                        "itemRatingValue": 4.7,
+                        "itemNoOfRatings": 21
+                    },
+                    "itemPrice": 200
+                },
+                {
+                    "itemImageLocation": "../assets/vendor_page_images/creamy_tomato_and_spinach_pasta.jpg",
+                    "itemId": "v4i3",
+                    "itemTitle": "Creamy Tomato and Spinach Pasta",
+                    "itemDescription": "With special pesto sauce and mozzarella cheese.",
+                    "itemRatings": {
+                        "itemRatingValue": 3.5,
+                        "itemNoOfRatings": 15
+                    },
+                    "itemPrice": 230
+                },
             ],
         },
     ],
